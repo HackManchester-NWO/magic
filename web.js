07942bfdd2f9c5844bcb1a2741118a6d5a20e960
shawkes <shawkes@gmail.com>
2013-10-26 23:04:05 +0100

    WOOOOOOOOOOORKS

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!!!!');
 });
 
+app.get('/in/', function(request, response) {
+  response.send('Hello SMS');
+});
+
 var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
