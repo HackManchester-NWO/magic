99834cc480e9d3f5b26e9cb20158da1d0fb93478
shawkes <shawkes@gmail.com>
2013-10-26 22:44:39 +0100

    change procfile

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var expect = require('expect.js');
   
 describe('Suite one', function(){
  it (function(done){
-   request.post('localhost:8080').end(function(res){
+   request.get('localhost:8888').end(function(res){
     expect(res).to.exist;
     expect(res.status).to.equal(200);
     expect(res.body).to.contain('world');
