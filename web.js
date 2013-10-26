09880129b6821d8fab2a160dbd8e7dcdc621e130
shawkes <shawkes@gmail.com>
2013-10-27 00:14:58 +0100

    WOOOOOOOOOOORKS

--- web.js
+++ web.js
@@ -1,12 +1,24 @@
 var express = require("express");
 var app = express();
+var http = require('http');var http = require('http');
 app.use(express.logger());
+var http = require('http');
 
 app.get('/', function(request, response) {
 	response.send('Hello World!!!!');
 });
 
 app.get('/in', function(request, response) {
+	var options = {
+	  host: 'api.clockworksms.com/',
+	  path: '/http/send.aspx?key=0b377aa9114a3c22a0ba512c6ac7f3af3110b8bb&to=447453847173&content=Hello+World'
+	};
+	http.get(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	  
+	}).on('error', function(e) {
+	  console.log('ERROR: ' + e.message);
+	});
 	response.send('Hello SMS');
 });
 
