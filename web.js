e7ec8f03b751d7a78bc729b82a62208fce447414
shawkes <shawkes@gmail.com>
2013-10-27 11:11:36 +0000

    texting!

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http');var http = require('http');
 app.use(express.logger());
 var http = require('http');
+var https = require('https');
+var redis_client = require("redis").createClient(2854,'50.30.35.9', {auth_pass:'6846153632dec6f9191442a0d881cac2'});
 
 app.get('/', function(request, response) {
 	response.send('Hello World!!!!');
@@ -10,23 +12,89 @@ app.get('/', function(request, response) {
 
 app.get('/in', function(request, response) {
 	var sentTextBody  = "Nothing :(";
+	var fromNumber = "";
+	var gitRefs;
 	if (request.query.content) {
-		sentTextBody = request.query.content
+		fromNumber = request.query.from;
 	}
-	var options = {
-	  host: 'api.clockworksms.com',
-	  path: '/http/send.aspx?key=0b377aa9114a3c22a0ba512c6ac7f3af3110b8bb&to=447453847173&content=' + encodeURIComponent(sentTextBody)
-	};
-	http.get(options, function(res) {
-	  console.log('STATUS: ' + res.statusCode );
-	  console.log('PATH: ' + options.path );
+	
+	if (request.query.content) {
+		sentTextBody = request.query.content;
+	}
+	// if (sentTextBody.toLowerCase() == "m") {
+		// redis_client.get(fromNumber, function (err, reply) {
+			// getReadMe(reply);
+		// });
+//	} else {
+		var gitRefs = sentTextBody.split(" ");
+		var chuckString = ""
+		redis_client.set(1, gitRefs, function (err, reply) {
+			https.get('https://raw.github.com/' + gitRefs[0] +'/' + gitRefs[1] + '/master/README.md', function(response) {
+				
+				response.on("data", function(chunk) {
+					chuckString = chuckString + chunk
+									
+				});
+				response.on("end", function() {
+					var thisPage = getPage(chuckString, 6);
+					console.log("getting paged text : " + getPage(chuckString, 6));
+					var sendSMSUrl = '/http/send.aspx?key=0b377aa9114a3c22a0ba512c6ac7f3af3110b8bb&to=447453847173&content=' + encodeURIComponent(thisPage);
+					var options = {
+						host: 'api.clockworksms.com', 
+						path: sendSMSUrl
+					};
+					console.log(options);
+					http.get(options, function(response) {
+					});
+				});
+			}).on('error', function(e) {
+			  console.log('ERROR: ' + e.message);
+			});
+			
+			
+			function getPage(unpagedText, pageNumber) {
+				//console.log("********************************************************************** HELLO IS IT BRIE YOU'RE LOOKING FOR? ");	
+				//console.log("************************************************************ " + unpagedText.replace(["^A-Za-z0-9@$_\/.,\"():;\-=+&%#!?<>' \n]","");
+				unpagedText = unpagedText.replace("\n", " ");
+				var smsLength = 142;
+				return  unpagedText.substr(pageNumber * smsLength, smsLength);
+			}
+		});
+	//}
+	
+	response.send('Hello SMS');
+	response.end();
+});
+
+
+
+
+function getReadMe(gitRefs, page) {
+	var theReadMe = ""
+	if (typeof page != 'undefined') {page = 0;}
+	
+		
+	https.get('https://raw.github.com/' + gitRefs[0] +'/' + gitRefs[1] + '/master/README.md', function(response) {
+		response.on("data", function(chunk) {
+			console.log("********************************************************************** HELLO IS IT BRIE YOU'RE LOOKING FOR? ", chunk);
+			// var options = {
+			  // host: 'api.clockworksms.com',
+			  // path: '/http/send.aspx?key=0b377aa9114a3c22a0ba512c6ac7f3af3110b8bb&to=447453847173&content=test')// + encodeURIComponent(theReadMe + chunk)
+			// };
+			// http.get(options, function(response) {
+			  // console.log('STATUS: ' + response.statusCode );
+			  // console.log('PATH: ' + options.path );
+			  // console.log('SMS sent!');
+			// }).on('error', function(e) {
+			  // console.log('ERROR: ' + e.message);
+			// });
+		});
 	}).on('error', function(e) {
 	  console.log('ERROR: ' + e.message);
 	});
-	response.send('Hello SMS');
-	response.end();
-	 console.log('SMS sent!');
-});
+}
+
+
 
 var port = process.env.PORT || 80;
 app.listen(port, function() {
